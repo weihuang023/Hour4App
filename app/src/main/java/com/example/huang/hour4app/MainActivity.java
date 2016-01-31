cda88e0cb8384de8e1dbc6e9ebaca60074233e9e
weihuang023 <weih81888@gmail.com>
2016-01-30 22:21:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/huang/hour4app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.huang.hour4app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
